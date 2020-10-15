ec4b855d9baa3375ca1333952afe06b7fa5a3f0a
Noah <amanda-ostman@hotmail.com>
2020-10-16 00:45:18 +0200

    2

---
+++ Qbus Microgame Snake/Assets/Script/NewMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovement : MonoBehaviour
+{
+    public float timerMax = 1f;
+    private float theTimer;
+
+    public GameObject qubus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theTimer = 0;
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+
+        {
+            qubus.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            qubus.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);
+        }
+        theTimer += Time.deltaTime;
+        if(theTimer >= timerMax)
+        {
+           
+            transform.position += transform.up;
+            theTimer = 0f;
+        }
+    }
+}
---
+++ Qbus Microgame Snake/Assets/Script/QubusV2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QubusV2 : MonoBehaviour
+{
+    private Vector2Int gridMoveDirection;
+    private Vector2Int gridPosition;
+    private float gridMoveTimer;
+    private float gridMovetTimerMax;
+
+    private void Awake()
+    {
+        gridPosition = new Vector2Int(5, 5);
+        gridMovetTimerMax = 1f;
+        gridMoveTimer = gridMovetTimerMax;
+        gridMoveDirection = new Vector2Int(1, 0);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gridMoveDirection.y = 0;
+            gridMoveDirection.x = +1;
+           
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            gridMoveDirection.y = 0;
+            gridMoveDirection.x = 1;
+        }
+
+        gridMoveTimer += Time.deltaTime;
+        if (gridMoveTimer >= gridMovetTimerMax)
+        {
+            gridPosition += gridMoveDirection;
+            gridMoveTimer -= gridMovetTimerMax;
+        }
+
+        transform.position = new Vector3(gridPosition.x, gridPosition.y);
+    }
+}
