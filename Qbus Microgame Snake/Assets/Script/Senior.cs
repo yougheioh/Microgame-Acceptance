209d3a3b2db8282f601574a116811b0d2418b623
Noah <amanda-ostman@hotmail.com>
2020-10-20 23:44:45 +0200

    Foood

--- Qbus Microgame Snake/Assets/Script/Senior.cs
+++ Qbus Microgame Snake/Assets/Script/Senior.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Senior : MonoBehaviour
 {
+    public GameObject senior;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D trigger)
-    
+
     {
+        SpawnSenior();
         Destroy(gameObject);
+    }
+
+    void SpawnSenior()
+        {
+      
+        int xPos = Random.Range(-13, 13);
+        int yPos = Random.Range(-9, 9);
+        GameObject a = Instantiate(senior) as GameObject;
+        a.transform.position = new Vector2(xPos, yPos);
+
     }
 }
